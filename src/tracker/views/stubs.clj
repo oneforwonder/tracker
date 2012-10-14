72eff922d0886550169c99a13344a613f4cd402e
Rob Nagle <rjn945@gmail.com>
2012-10-13 19:11:04 -0700

    Some stubs

---
+++ src/tracker/views/stubs.clj
@@ -0,0 +1,47 @@
+(ns tracker.views.stubs
+  (:require [tracker.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core]
+        [hiccup.core :only [html]]))
+
+(defpage "/todo" []
+  (common/layout {}
+    [:h1 "Todo"]
+    [:h3 "Tasks:"]
+    [:ul
+     [:li "Improve Tracker"]]))
+
+(defpage "/exercise" []
+  (common/layout {}
+    [:h1 "Exercise"]
+    [:p "Lift something heavy"]))
+
+(defpage "/financial" []
+  (common/layout {}
+    [:h1 "Financial"]
+    [:p "Make some money. Buy some things."]))
+
+(defpage "/programming" []
+  (common/layout {}
+    [:h1 "Programming"]
+    [:p "TODO: This page"]))
+
+(defpage "/music" []
+  (common/layout {}
+    [:h1 "Music"]
+    [:p "Write the next great electronic rock opera."]))
+
+(defpage "/mind" []
+  (common/layout {}
+    [:h1 "Mind"]
+    [:p "Read a book."]))
+
+(defpage "/mind" []
+  (common/layout {}
+    [:h1 "Mind"]
+    [:p "Read a book."]))
+
+(defpage "/social" []
+  (common/layout {}
+    [:h1 "Social"]
+    [:p "Talk to someone."]))
